dc4c7659dcc7baefc64a074d22f685bb9291da6b
Amir Zur <amir.zur@hotmail.com>
2024-11-01 15:16:40 -0700

    run DAS with 1 dim, see what happens??

--- das_experiments.sh
+++ das_experiments.sh
@@ -1,4 +1,4 @@
-nlprun -a pyvene -g 1 -r 40G -q jag -o logs/0_300_fc1_i2.log 'python das_experiments.py --start_index=0 --end_index=300 --intervention_size 2'
+nlprun -a pyvene -g 1 -r 40G -q jag -o logs/0_300_i1.log 'python das_experiments.py --start_index=0 --end_index=300 --intervention_size 1'
 # nlprun -a pyvene -g 1 -r 40G -q jag -o logs/30_60_i8.log 'python das_experiments.py --start_index=30 --end_index=60 --intervention_size 8'
 # nlprun -a pyvene -g 1 -r 40G -q jag -o logs/0_30_i16.log 'python das_experiments.py --start_index=0 --end_index=30 --intervention_size 16'
 # nlprun -a pyvene -g 1 -r 40G -q jag -o logs/30_60_i16.log 'python das_experiments.py --start_index=30 --end_index=60 --intervention_size 16'
