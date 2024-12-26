cbcd23725db6d9240edd54e98d8799290f1c0112
Amir Zur <amir.zur@hotmail.com>
2024-12-25 17:49:32 -0800

    run DAS + probing on ambiguous models

--- das_experiments.sh
+++ das_experiments.sh
@@ -1,15 +1,25 @@
-# 30 40 50
-nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_30_40_50_i4.log 'python das_experiments.py --coefficients 30_40_50 --intervention_size 4'
-nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_30_40_50_i8.log 'python das_experiments.py --coefficients 30_40_50 --intervention_size 8'
-nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_30_40_50_i16.log 'python das_experiments.py --coefficients 30_40_50 --intervention_size 16'
-nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_30_40_50_i32.log 'python das_experiments.py --coefficients 30_40_50 --intervention_size 32'
-# 40 40 40
-nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_40_40_40_i4.log 'python das_experiments.py --coefficients 40_40_40 --intervention_size 4'
-nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_40_40_40_i8.log 'python das_experiments.py --coefficients 40_40_40 --intervention_size 8'
-nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_40_40_40_i16.log 'python das_experiments.py --coefficients 40_40_40 --intervention_size 16'
-nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_40_40_40_i32.log 'python das_experiments.py --coefficients 40_40_40 --intervention_size 32'
-# 50 40 30
-nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_50_40_30_i4.log 'python das_experiments.py --coefficients 50_40_30 --intervention_size 4'
-nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_50_40_30_i8.log 'python das_experiments.py --coefficients 50_40_30 --intervention_size 8'
-nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_50_40_30_i16.log 'python das_experiments.py --coefficients 50_40_30 --intervention_size 16'
-nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_50_40_30_i32.log 'python das_experiments.py --coefficients 50_40_30 --intervention_size 32'
+# # 30 40 50
+# nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_30_40_50_i4.log 'python das_experiments.py --coefficients 30_40_50 --intervention_size 4'
+# nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_30_40_50_i8.log 'python das_experiments.py --coefficients 30_40_50 --intervention_size 8'
+# nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_30_40_50_i16.log 'python das_experiments.py --coefficients 30_40_50 --intervention_size 16'
+# nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_30_40_50_i32.log 'python das_experiments.py --coefficients 30_40_50 --intervention_size 32'
+# # 40 40 40
+# nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_40_40_40_i4.log 'python das_experiments.py --coefficients 40_40_40 --intervention_size 4'
+# nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_40_40_40_i8.log 'python das_experiments.py --coefficients 40_40_40 --intervention_size 8'
+# nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_40_40_40_i16.log 'python das_experiments.py --coefficients 40_40_40 --intervention_size 16'
+# nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_40_40_40_i32.log 'python das_experiments.py --coefficients 40_40_40 --intervention_size 32'
+# # 50 40 30
+# nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_50_40_30_i4.log 'python das_experiments.py --coefficients 50_40_30 --intervention_size 4'
+# nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_50_40_30_i8.log 'python das_experiments.py --coefficients 50_40_30 --intervention_size 8'
+# nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_50_40_30_i16.log 'python das_experiments.py --coefficients 50_40_30 --intervention_size 16'
+# nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_50_40_30_i32.log 'python das_experiments.py --coefficients 50_40_30 --intervention_size 32'
+# 36 40 44
+nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_36_40_44_i4.log 'python das_experiments.py --coefficients 36_40_44 --intervention_size 4'
+nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_36_40_44_i8.log 'python das_experiments.py --coefficients 36_40_44 --intervention_size 8'
+nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_36_40_44_i16.log 'python das_experiments.py --coefficients 36_40_44 --intervention_size 16'
+nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_36_40_44_i32.log 'python das_experiments.py --coefficients 36_40_44 --intervention_size 32'
+# 44 40 36
+nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_44_40_36_i4.log 'python das_experiments.py --coefficients 44_40_36 --intervention_size 4'
+nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_44_40_36_i8.log 'python das_experiments.py --coefficients 44_40_36 --intervention_size 8'
+nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_44_40_36_i16.log 'python das_experiments.py --coefficients 44_40_36 --intervention_size 16'
+nlprun -a ellipse -g 1 -r 40G -q jag -o logs/das_44_40_36_i32.log 'python das_experiments.py --coefficients 44_40_36 --intervention_size 32'
--- probe_experiments.sh
+++ probe_experiments.sh
@@ -1,3 +1,5 @@
-nlprun -a ellipse -g 1 -r 40G -q jag -o logs/probe_30_40_50.log 'python probe_experiments.py --coefficients 30_40_50'
-nlprun -a ellipse -g 1 -r 40G -q jag -o logs/probe_40_40_40.log 'python probe_experiments.py --coefficients 40_40_40'
-nlprun -a ellipse -g 1 -r 40G -q jag -o logs/probe_50_40_30.log 'python probe_experiments.py --coefficients 50_40_30'
+# nlprun -a ellipse -g 1 -r 40G -q jag -o logs/probe_30_40_50.log 'python probe_experiments.py --coefficients 30_40_50'
+# nlprun -a ellipse -g 1 -r 40G -q jag -o logs/probe_40_40_40.log 'python probe_experiments.py --coefficients 40_40_40'
+# nlprun -a ellipse -g 1 -r 40G -q jag -o logs/probe_50_40_30.log 'python probe_experiments.py --coefficients 50_40_30'
+nlprun -a ellipse -g 1 -r 40G -q jag -o logs/probe_36_40_44.log 'python probe_experiments.py --coefficients 36_40_44'
+nlprun -a ellipse -g 1 -r 40G -q jag -o logs/probe_44_40_36.log 'python probe_experiments.py --coefficients 44_40_36'
